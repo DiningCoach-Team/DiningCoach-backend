dfb24117fabfac092b01c97714946c6706d5ab33
Hyung Goo Lee <hgl9023@naver.com>
2023-05-01 16:58:17 +0900

    Fix: Hide Django Secret Key

--- diningcoach/diningcoach/settings.py
+++ diningcoach/diningcoach/settings.py
@@ -11,16 +11,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5o+4!kc@z3a!uaosj9^hzqt_r8x&(_nom901t2@e&)mxay2#24'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
