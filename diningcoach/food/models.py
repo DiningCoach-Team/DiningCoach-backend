10a7ef1ab7cfb5a36bee990afbe794a5ba29c48e
Hyung Goo Lee <hgl9023@naver.com>
2023-05-11 12:57:42 +0900

    Feat: Add 'ProcessedFood' model

--- diningcoach/food/models.py
+++ diningcoach/food/models.py
@@ -19,3 +19,24 @@ class CustomizedFood(models.Model):
 
   def __str__(self):
     return '[Customized Food] ' + self.name
+
+
+class ProcessedFood(models.Model):
+  food_image = models.TextField(verbose_name='식품 사진', blank=True, null=True)
+  food_category = models.CharField(verbose_name='식품 분류', max_length=255)
+  name = models.CharField(verbose_name='식품명', max_length=255)
+  country = models.CharField(verbose_name='제조국', max_length=255, blank=True, null=True)
+  brand_name = models.CharField(verbose_name='제조사명', max_length=255, blank=True, null=True)
+  report_no = models.IntegerField(verbose_name='품목보고번호', blank=True, null=True)
+  food_kind = models.CharField(verbose_name='식품유형', max_length=255, blank=True, null=True)
+  allergy_info = models.TextField(verbose_name='알레르기 정보', blank=True, null=True)
+  storage = models.TextField(verbose_name='보관방법', blank=True, null=True)
+  barcode = models.IntegerField(verbose_name='유통바코드', blank=True, null=True)
+  amount_per_serving = models.IntegerField(verbose_name='1회제공량', blank=True, null=True)
+
+  class Meta:
+    verbose_name = '가공식품'
+    verbose_name_plural = verbose_name
+
+  def __str__(self):
+    return '[Processed Food] ' + self.name
