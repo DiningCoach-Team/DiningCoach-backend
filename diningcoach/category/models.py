89c189e152ee79155da8d8d80e62c25d3ad3ed8f
Hyung Goo Lee <hgl9023@naver.com>
2023-05-19 18:15:31 +0900

    Feat: Add 'FoodCategory' model

--- diningcoach/category/models.py
+++ diningcoach/category/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class FoodCategory(models.Model):
+  category_name = models.CharField(verbose_name='카테고리명', max_length=255)
+
+  class Meta:
+    verbose_name = '식품분류 카테고리'
+    verbose_name_plural = verbose_name
+
+  def __str__(self):
+    return '[Food Category] ' + self.category_name
