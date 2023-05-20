7404e7798cec658e3c4438fe94381fc28d63e917
Hyung Goo Lee <hgl9023@naver.com>
2023-05-20 20:25:06 +0900

    Feat: Add 'PreferenceCategory' model

--- diningcoach/category/models.py
+++ diningcoach/category/models.py
@@ -10,3 +10,14 @@ class FoodCategory(models.Model):
 
   def __str__(self):
     return '[Food Category] ' + self.category_name
+
+
+class PreferenceCategory(models.Model):
+  category_name = models.CharField(verbose_name='카테고리명', max_length=255)
+
+  class Meta:
+    verbose_name = '선호음식 카테고리'
+    verbose_name_plural = verbose_name
+
+  def __str__(self):
+    return '[Preference Category] ' + self.category_name
