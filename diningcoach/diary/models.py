e7b73ddcbe99019e8c15f0d6af74571b6e2b80a5
Hyung Goo Lee <hgl9023@naver.com>
2023-05-23 17:13:13 +0900

    Feat: Add 'Diary' model

--- diningcoach/diary/models.py
+++ diningcoach/diary/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Diary(models.Model):
+  date = models.DateField(verbose_name='날짜')
+  is_deleted = models.BooleanField(verbose_name='삭제 여부', default=False)
+  user = models.ForeignKey(User, verbose_name='회원', on_delete=models.CASCADE)
+
+  class Meta:
+    verbose_name = '식단일기'
+    verbose_name_plural = verbose_name
+
+  def __str__(self):
+    return '[Diary] ' + str(self.date)
