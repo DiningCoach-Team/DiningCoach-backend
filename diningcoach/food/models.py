e87ff8a7a45c4039d24bdf02b708b22f543dc541
Hyung Goo Lee <hgl9023@naver.com>
2024-01-06 20:09:03 +0900

    Fix: Modify 'CustomizedFood' model

--- diningcoach/food/models.py
+++ diningcoach/food/models.py
@@ -48,14 +48,7 @@ class TimestampModel(models.Model):
 
 
 #################### 식품 ####################
-class CustomizedFood(models.Model):
-  food_type = models.CharField(verbose_name='식품 종류', max_length=45)
-  food_category = models.CharField(verbose_name='식품 분류', max_length=255, blank=True, null=True)
-  name = models.CharField(verbose_name='식품명', max_length=255)
-  brand_name = models.CharField(verbose_name='제조사명', max_length=255, blank=True, null=True)
-  barcode = models.IntegerField(verbose_name='유통바코드', blank=True, null=True)
-  amount_per_serving = models.IntegerField(verbose_name='1회제공량', blank=True, null=True)
-  created_at = models.DateTimeField(verbose_name='생성 일시', auto_now_add=True)
+class CustomizedFood(FoodModel):
   user = models.ForeignKey(User, verbose_name='회원', on_delete=models.CASCADE)
 
   class Meta:
@@ -63,7 +56,7 @@ class CustomizedFood(models.Model):
     verbose_name_plural = verbose_name
 
   def __str__(self):
-    return '[Customized Food] ' + self.name
+    return '사용자 추가식품 : ' + self.food_name
 
 
 class ProcessedFood(models.Model):
