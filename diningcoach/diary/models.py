67b5e25c8b5b74eb41e2d1cae37c47525798119a
Hyung Goo Lee <hgl9023@naver.com>
2024-01-28 16:19:25 +0900

    Fix: Modify 'Meal' model

--- diningcoach/diary/models.py
+++ diningcoach/diary/models.py
@@ -40,25 +40,28 @@ class Diary(models.Model):
     return '[Diary] ' + str(self.date)
 
 
-class Meal(models.Model):
+#################### 식단일기 ####################
+class Meal(TimestampModel):
   MEAL_TYPES = [
-    (1, "Breakfast"),
-    (2, "Lunch"),
-    (3, "Dinner"),
-    (4, "Snack")
+    (1, 'Breakfast'),
+    (2, 'Lunch'),
+    (3, 'Dinner'),
+    (4, 'Snack'),
   ]
 
-  meal_type = models.CharField(verbose_name='식사 종류', max_length=45, choices=MEAL_TYPES)
-  content = models.TextField(verbose_name='내용', blank=True, null=True)
-  is_deleted = models.BooleanField(verbose_name='삭제 여부', default=False)
-  diary = models.ForeignKey(Diary, verbose_name='식단일기', on_delete=models.CASCADE)
+  date         = models.DateField(verbose_name='식단일기 날짜')
+  meal_type    = models.CharField(verbose_name='식사 종류', max_length=50, choices=MEAL_TYPES)
+  content      = models.TextField(verbose_name='내용', blank=True, null=True)
+  is_favourite = models.BooleanField(verbose_name='즐겨찾기', default=False)
+  is_deleted   = models.BooleanField(verbose_name='삭제 여부', default=False)
+  user         = models.ForeignKey(User, verbose_name='회원', on_delete=models.CASCADE)
 
   class Meta:
     verbose_name = '식사'
     verbose_name_plural = verbose_name
 
   def __str__(self):
-    return '[Meal] ' + str(self.diary.date) + ' ' + self.meal_type
+    return '식사 : ' + str(self.date) + ' ' + self.meal_type
 
 
 class MealImage(models.Model):
