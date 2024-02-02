1fc6d6b6838f41942c1e763d12e76b174f71464c
Hyung Goo Lee <hgl9023@naver.com>
2024-02-02 14:37:20 +0900

    Fix: Remove 'Diary' model

--- diningcoach/diary/models.py
+++ diningcoach/diary/models.py
@@ -27,19 +27,6 @@ class NutritionModel(models.Model):
     abstract = True
 
 
-class Diary(models.Model):
-  date = models.DateField(verbose_name='날짜')
-  is_deleted = models.BooleanField(verbose_name='삭제 여부', default=False)
-  user = models.ForeignKey(User, verbose_name='회원', on_delete=models.CASCADE)
-
-  class Meta:
-    verbose_name = '식단일기'
-    verbose_name_plural = verbose_name
-
-  def __str__(self):
-    return '[Diary] ' + str(self.date)
-
-
 #################### 식단일기 ####################
 class Meal(TimestampModel):
   MEAL_TYPES = [
