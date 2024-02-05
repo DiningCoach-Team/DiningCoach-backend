580a89fe9429c0149f8de4ebcf8cb4ee0348b6f1
Hyung Goo Lee <hgl9023@naver.com>
2024-02-05 21:35:10 +0900

    Feat: Add PostgreSQL connection settings

--- diningcoach/diningcoach/settings.py
+++ diningcoach/diningcoach/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'diningcoach.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRESQL_NAME'),
+        'USER': env('POSTGRESQL_USER'),
+        'PASSWORD': env('POSTGRESQL_PASSWORD'),
+        'HOST': env('POSTGRESQL_HOST'),
+        'PORT': env('POSTGRESQL_PORT')
     }
 }
 
