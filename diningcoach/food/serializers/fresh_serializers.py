3525db1b8547ef1dbb466469628ff763bbd75604
Hyung Goo Lee <hgl9023@naver.com>
2024-03-18 22:57:28 +0900

    Fix: Modify fresh food serializers

--- diningcoach/food/serializers/fresh_serializers.py
+++ diningcoach/food/serializers/fresh_serializers.py
@@ -3,19 +3,27 @@ from food.models import FreshFood, FreshNutrition
 from rest_framework import serializers
 
 
+class FreshNutritionSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = FreshNutrition
+    exclude = ['fresh_food']
+
+
+class FreshFoodDefaultSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = FreshFood
+    fields = '__all__'
+
+
 class FreshFoodSimpleSerializer(serializers.ModelSerializer):
   class Meta:
     model = FreshFood
     fields = ['id', 'food_name', 'category_main', 'category_sub', 'food_image', 'harvest_time']
 
 
-class FreshFoodSerializer(serializers.ModelSerializer):
+class FreshFoodDetailSerializer(serializers.ModelSerializer):
+  nutrition_info = FreshNutritionSerializer(many=False, read_only=True)
+
   class Meta:
     model = FreshFood
     fields = '__all__'
-
-
-class FreshNutritionSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = FreshNutrition
-    exclude = ['fresh_food']
