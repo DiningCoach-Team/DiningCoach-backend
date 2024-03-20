80cf7d8ada699dadb6d76602082b5fe813c12325
Hyung Goo Lee <hgl9023@naver.com>
2024-03-20 12:41:32 +0900

    Feat: Add FoodSearchSerializer for later use

--- diningcoach/food/serializers/serializers.py
+++ diningcoach/food/serializers/serializers.py
@@ -1,30 +1,13 @@
 from food.models import ProcessedFood, FreshFood, CookedFood
-from food.serializers.processed_serializers import ProcessedNutritionSerializer
-from food.serializers.fresh_serializers import FreshNutritionSerializer
-from food.serializers.cooked_serializers import CookedNutritionSerializer
+from food.serializers.processed_serializers import ProcessedFoodSimpleSerializer
+from food.serializers.fresh_serializers import FreshFoodSimpleSerializer
+from food.serializers.cooked_serializers import CookedFoodSimpleSerializer
 
 from rest_framework import serializers
 
 
-class ProcessedFoodDetailSerializer(serializers.ModelSerializer):
-  nutrition_info = ProcessedNutritionSerializer(many=False, read_only=True)
-
-  class Meta:
-    model = ProcessedFood
-    fields = '__all__'
-
-
-class FreshFoodDetailSerializer(serializers.ModelSerializer):
-  nutrition_info = FreshNutritionSerializer(many=False, read_only=True)
-
-  class Meta:
-    model = FreshFood
-    fields = '__all__'
-
-
-class CookedFoodDetailSerializer(serializers.ModelSerializer):
-  nutrition_info = CookedNutritionSerializer(many=False, read_only=True)
-
-  class Meta:
-    model = CookedFood
-    fields = '__all__'
+# Not used
+class FoodSearchSerializer(serializers.Serializer):
+  processed_food = ProcessedFoodSimpleSerializer(many=True, read_only=True)
+  fresh_food = FreshFoodSimpleSerializer(many=True, read_only=True)
+  cooked_food = CookedFoodSimpleSerializer(many=True, read_only=True)
