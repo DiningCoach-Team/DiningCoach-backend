7af15dee8b285a2f02a15033f5a458244c5ab4ca
Hyung Goo Lee <hgl9023@naver.com>
2024-03-21 22:11:46 +0900

    Feat: GET 'api/food/search/processed' API

--- diningcoach/food/urls.py
+++ diningcoach/food/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from food.views import (
-  FoodScanView, FoodSearchView
+  FoodScanView, ProcessedFoodSearchView
 )
 
 urlpatterns = [
   path('scan/<str:barcode_no>', FoodScanView.as_view()),
-  path('search', FoodSearchView.as_view()), # query parameter : type, code, name, main, sub
+
+  # query parameter : no, code, name, cate_main, cate_sub
+  path('search/processed', ProcessedFoodSearchView.as_view()),
+  # path('search/fresh', FreshFoodSearchView.as_view()),
+  # path('search/cooked', CookedFoodSearchView.as_view()),
+
+  # path('detail/processed', ProcessedFoodDetailView.as_view()),
+  # path('detail/fresh', FreshFoodDetailView.as_view()),
+  # path('detail/cooked', CookedFoodDetailView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- diningcoach/food/views.py
+++ diningcoach/food/views.py
@@ -3,26 +3,27 @@ from django.shortcuts import render
 from django.core.exceptions import ValidationError
 
 from food.models import ProcessedFood
-from food.serializers.serializers import ProcessedFoodDetailSerializer
+from food.serializers.processed_serializers import ProcessedFoodSimpleSerializer, ProcessedFoodDetailSerializer
 from food.exceptions import InvalidInputFormatException, NoResultFoundException
 
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet, NumberFilter, CharFilter
 
 
 # api/food/scan/<str:barcode_no>
 class FoodScanView(ListAPIView):
   serializer_class = ProcessedFoodDetailSerializer
 
-  def validate(self, barcode_no):
+  def validate_input(self, barcode_no):
     if not barcode_no.isnumeric():
       raise InvalidInputFormatException(detail='스캔하신 바코드의 입력 형식이 올바르지 않습니다.', code=status.HTTP_400_BAD_REQUEST)
 
   def get_queryset(self):
     barcode_no = self.kwargs['barcode_no']
-    self.validate(barcode_no)
+    self.validate_input(barcode_no)
 
     return ProcessedFood.objects.filter(barcode_no=barcode_no)
 
@@ -47,7 +48,40 @@ class FoodScanView(ListAPIView):
   #   return Response(serializer.data)
 
 
-# api/food/search/?
-class FoodSearchView(APIView):
-  def get(self):
-    pass
+class FoodFilter(FilterSet):
+  no = NumberFilter(field_name='id', lookup_expr='exact')
+  code = CharFilter(field_name='food_code', lookup_expr='contains')
+  name = CharFilter(field_name='food_name', lookup_expr='contains')
+  cate_main = CharFilter(field_name='category_main', lookup_expr='iexact')
+  cate_sub = CharFilter(field_name='category_sub', lookup_expr='iexact')
+
+
+class ProcessedFoodFilter(FoodFilter):
+  class Meta:
+    model = ProcessedFood
+    fields = ['no', 'code', 'name', 'cate_main', 'cate_sub']
+
+
+# api/food/search/processed?no=?&code=?&name=?&cate_main=?&cate_sub=?
+class ProcessedFoodSearchView(ListAPIView):
+  queryset = ProcessedFood.objects.all()
+  serializer_class = ProcessedFoodSimpleSerializer
+  filter_backends = [DjangoFilterBackend]
+  filterset_class = ProcessedFoodFilter
+
+  def validate_input(self):
+    correct_query_params = ['no', 'code', 'name', 'cate_main', 'cate_sub']
+    input_query_params = list(self.request.query_params.keys())
+    if not all(key in correct_query_params for key in input_query_params):
+      raise InvalidInputFormatException(detail='입력하신 검색 조건 인자가 올바르지 않습니다.', code=status.HTTP_400_BAD_REQUEST)
+
+  def list(self, request, *args, **kwargs):
+    self.validate_input()
+
+    queryset = self.filter_queryset(self.get_queryset())
+
+    if not queryset.exists():
+      raise NoResultFoundException(detail='검색하신 조건에 해당하는 가공식품이 존재하지 않습니다.', code=status.HTTP_204_NO_CONTENT)
+
+    serializer = self.get_serializer(queryset, many=True)
+    return Response(serializer.data)
