4c931cd198f90469550b1ab4efb2a94bb090c84c
Hyung Goo Lee <hgl9023@naver.com>
2024-03-27 18:07:01 +0900

    Feat: Add helper class for 'User' model

--- diningcoach/user/models.py
+++ diningcoach/user/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
 import uuid
 
 
@@ -9,6 +10,38 @@ class TimestampModel(models.Model):
 
   class Meta:
     abstract = True
+
+
+##### 헬퍼클래스 #####
+class UserManager(BaseUserManager):
+  use_in_migrations = True
+
+  def create_user(self, email, password, **extra_fields):
+    extra_fields.setdefault('is_staff', False)
+    extra_fields.setdefault('is_superuser', False)
+    extra_fields.setdefault('is_active', False)
+
+    if not email:
+      raise ValueError('User email must be set.')
+    email = self.normalize_email(email)
+
+    user = self.model(email=email, **extra_fields)
+    user.set_password(password)
+    user.save(using=self._db)
+    
+    return user
+
+  def create_superuser(self, email=None, password=None, **extra_fields):
+    extra_fields.setdefault('is_staff', True)
+    extra_fields.setdefault('is_superuser', True)
+    extra_fields.setdefault('is_active', True)
+
+    if extra_fields.get('is_staff') is not True:
+      raise ValueError('Superuser must have is_staff=True.')
+    if extra_fields.get('is_superuser') is not True:
+      raise ValueError('Superuser must have is_superuser=True.')
+
+    return self.create_user(email, password, **extra_fields)
 
 
 ##### 회원 #####
