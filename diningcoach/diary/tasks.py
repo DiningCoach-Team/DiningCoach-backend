9815efe7e6530e19df7dc015409d6d7683283fe2
Hyung Goo Lee <hgl9023@naver.com>
2024-04-16 14:57:46 +0900

    Feat: Add feature to permanently delete meal diary in trash

--- diningcoach/diary/tasks.py
+++ diningcoach/diary/tasks.py
@@ -1,8 +1,13 @@
+import os
+import datetime
+
+from django.utils import timezone
 from django.db import transaction, IntegrityError
+from django.conf import settings
 
 from food.models import ProcessedFood, FreshFood, CookedFood
-from diary.models import MealFood, MealNutrition
-from diary.exceptions import CreateDataFailedException, UpdateDataFailedException
+from diary.models import MealDiary, MealFood, MealNutrition
+from diary.exceptions import CreateDataFailedException, UpdateDataFailedException, DeleteDataFailedException
 
 from rest_framework import serializers
 
@@ -110,3 +115,64 @@ def edit_meal_nutrition(meal_diary_id):
     meal_nutrition_serializer.save()
   except IntegrityError:
     raise UpdateDataFailedException(detail=('UPDATE_DATA_FAILED', '식단일기 영양성분 데이터 수정에 실패하였습니다.'))
+
+
+def delete_meal_image(meal_image_list):
+  if meal_image_list is None or not meal_image_list.exists():
+    return
+
+  meal_image_list = list(meal_image_list)
+  for meal_image_instance in meal_image_list:
+    image_file_path = os.path.join(settings.MEDIA_ROOT, str(meal_image_instance.image_url))
+    if os.path.exists(image_file_path):
+      os.remove(image_file_path)
+
+    meal_image_instance.delete()
+
+
+def delete_meal_food(meal_food_list):
+  if meal_food_list is None or not meal_food_list.exists():
+    return
+
+  meal_food_list = list(meal_food_list)
+  for meal_food_instance in meal_food_list:
+    meal_food_instance.delete()
+
+
+def delete_meal_nutrition(meal_nutrition_instance):
+  if meal_nutrition_instance is None:
+    return
+
+  meal_nutrition_instance.delete()
+
+
+@transaction.atomic
+def delete_meal_diary():
+  current_time = timezone.now()
+  thirty_days = datetime.timedelta(days=30)
+
+  try:
+    meal_diary_list = MealDiary.objects.select_for_update().filter(
+      is_deleted__exact=True,
+      updated_at__lt=(current_time - thirty_days),
+    ).prefetch_related(
+      'meal_image',
+      'meal_food',
+      'meal_nutrition',
+    )
+
+    if not meal_diary_list.exists():
+      return
+
+    meal_diary_list = list(meal_diary_list)
+    for meal_diary in meal_diary_list:
+      if hasattr(meal_diary, 'meal_image'):
+        delete_meal_image(meal_diary.meal_image.all())
+      if hasattr(meal_diary, 'meal_food'):
+        delete_meal_food(meal_diary.meal_food.all())
+      if hasattr(meal_diary, 'meal_nutrition'):
+        delete_meal_nutrition(meal_diary.meal_nutrition)
+
+      meal_diary.delete()
+  except IntegrityError:
+    raise DeleteDataFailedException(detail=('DELETE_DATA_FAILED', '식단일기 데이터 영구 삭제에 실패하였습니다.'))
