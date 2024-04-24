b1fece4d76d322eb0067eff5c4a7175b2f0f8aea
Hyung Goo Lee <hgl9023@naver.com>
2024-04-24 20:53:16 +0900

    Feat: Collect static files into a single directory

--- diningcoach/diningcoach/settings.py
+++ diningcoach/diningcoach/settings.py
@@ -167,8 +167,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'static'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 # Media files
